2875697d96276046c54be2b3d7110821ba79fb45
beaulazear <beaulazear@gmail.com>
2022-09-21 16:51:35 -0400

    commit tree a0abd0b78709c5bc5819cb53fc231acc0b3225b6

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+const distance = n;
+
+function scuberGreetingForFeet(n){
+   if (n <= 400)
+   return ("This one is on me!");
+   else if (n <= 2000)
+   return ("That will be twenty bucks.");
+   else if (n <= 2500)
+   return ("I will gladly take your thirty bucks.");
+   else if (n > 2500)
+   return ("No can do.");
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+const city = j;
+
+function ternaryCheckCity(j){
+  const canTakeRide = j === "NYC" ? "Ok, sounds good." : "No go.";
+  return canTakeRide;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+const tip = i;
+
+function switchOnCharmFromTip(i){
+switch (i) {
+  case "generous":
+    return ("Thank you so much.");
+  case ("not as generous"):
+    return ("Thank you.");
+  default:
+    return "Bye.";
+}
 }
